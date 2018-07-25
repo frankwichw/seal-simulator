9ab78d5f9c94015d8ba0f6d06a1c885baa01990c
frankwichw <frankwichw@gmail.com>
2018-07-25 14:52:01 -0400

    added click count

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import sealJSON from './seals.json';
+import Button from './components/Button/button.js';
 
 class App extends Component {
 
@@ -8,19 +9,42 @@ class App extends Component {
     super(props);
     this.state = {
       sealPic: sealJSON[0].sealBasicPic,
-      htmlTest: "<a href=\"http://google.com\">Google</a>"
+      approachClickCount: 0,
+      backUpClickCount: 0
     };
   };
 
-  render() {
+  handleSetStateAngry = () => {
+    const newCount = this.state.approachClickCount + 1;
+    this.setState({approachClickCount: newCount});
+    this.setState({sealPic: sealJSON[1].sealAngryPic});
+  };
 
+  handleSetStateNeutral = () => {
+    const newCount = this.state.backUpClickCount + 1;
+    this.setState({backUpClickCount: newCount});
+    this.setState({sealPic: sealJSON[0].sealBasicPic});
+  };
+
+  render() {
+    console.log("approach: " + this.state.approachClickCount);
+    console.log("back up: " + this.state.backUpClickCount);
     return (
       <div className="container">
         <div className="seal-pic" dangerouslySetInnerHTML={{__html: this.state.sealPic}}>
         </div>
+        <Button 
+          name="APROACH"
+          onClick={this.handleSetStateAngry}
+        />
+
+        <Button 
+          name="BACK UP"
+          onClick={this.handleSetStateNeutral}
+        />
       </div>
     );
-    
+
   }
 }
 
---
+++ src/components/Button/button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = props => (
+    <div className="button" onClick={props.onClick}>
+        &#8988; _______ &#8989;<br />
+        | {props.name} | <br />
+        &#8990; _______ &#8991;
+    </div>
+);
+
+export default Button;
